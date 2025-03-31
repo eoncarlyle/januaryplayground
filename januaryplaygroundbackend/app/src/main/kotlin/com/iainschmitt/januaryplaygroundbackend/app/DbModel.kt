c706393ae7f1026c3a989c7e31f5757704207fc6
eoncarlyle <me@iainschmitt.com>
2025-03-30 21:55:52 -0500

    Forgotten classes

---
+++ januaryplaygroundbackend/app/src/main/kotlin/com/iainschmitt/januaryplaygroundbackend/app/DbModel.kt
@@ -0,0 +1,37 @@
+package com.iainschmitt.januaryplaygroundbackend.app
+
+import com.iainschmitt.januaryplaygroundbackend.shared.OrderRequest
+import com.iainschmitt.januaryplaygroundbackend.shared.Ticker
+
+data class TickerRecord(
+    val ticker: Ticker,
+    private val openColumn: Int
+) {
+    val open = openColumn != 0
+}
+
+data class FilledOrderRecord(
+    val positionId: Long,
+    val filledTick: Long
+)
+
+data class LimitPendingOrderRecord(
+    val orderId: Long,
+    val receivedTick: Long
+)
+
+data class DeleteAllPositionsRecord(
+    val cancelledTick: Long,
+    val orderCount: Int
+)
+
+data class ValidOrderRecord<T: OrderRequest>(
+    val order: T,
+    val userBalance: Int
+)
+
+data class Quote(
+    val ticker: Ticker,
+    val bid: Int,
+    val ask: Int
+)
---
+++ januaryplaygroundbackend/app/src/main/kotlin/com/iainschmitt/januaryplaygroundbackend/app/TransactionSemaphores.kt
@@ -0,0 +1,32 @@
+package com.iainschmitt.januaryplaygroundbackend.app
+
+import com.iainschmitt.januaryplaygroundbackend.shared.Ticker
+import java.util.AbstractMap.SimpleEntry
+import java.util.concurrent.Semaphore
+
+class TransactionSemaphores(
+    private val db: DatabaseHelper
+) {
+    private val tickers = getAllTickers()
+    private val semaphores: Map<Ticker, Semaphore> =
+        tickers.map { ticker -> SimpleEntry(ticker, Semaphore(1)) }.associate { it.key to it.value }
+
+    private fun getAllTickers(): List<Ticker> {
+        return db.query { conn ->
+            val tickers = mutableListOf<TickerRecord>()
+            conn.prepareStatement("select symbol, open from ticker").use { stmt ->
+                stmt.executeQuery().use { rs ->
+                    while (rs.next()) {
+                        tickers.add(TickerRecord(rs.getString("symbol"), rs.getInt("open")))
+                    }
+                }
+            }
+            tickers
+        }.map { record -> record.ticker }
+    }
+
+
+    fun getSemaphore(ticker: Ticker): Semaphore? {
+        return semaphores[ticker]
+    }
+}
